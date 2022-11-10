82905d5cd2f5557f08e10a9e358ee09ac50109db
ariggi-epam <alberto_riggi@epam.com>
2022-11-10 17:33:28 -0600

    added api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      var text  = await (await fetch(`/api/message`)).json();
+      console.log(text)
+      setData(text.message);
+      
+    })();
+  });
+
+  return <div>hola {data}</div>;
 }
 
 export default App;
